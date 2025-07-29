5d1a677aab36f9e0f1ea9ddaa0d618d2676514c9
Juan Antonio <juananthonio23@gmail.com>
2025-07-29 12:54:40 +0700

    feat(Dashboard): implement delivery count logic based on user role

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,12 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return Inertia::render('dashboard');
+        $userId = Auth::id();
+        $userRole = Auth::user()->roles->first()->name;
+        $isAdmin = $userRole === 'admin' || $userRole === 'super admin';
+
+        $totalDeliveries = \App\Models\Delivery::when(!$isAdmin, function ($query) use ($userId) {
+            return $query->where('user_id', $userId);
+        })->count();
+
+        // $totalTrips = \App\Models\Trip::when(!$isAdmin, function ($query) use ($userId) {
+        //     return $query->where('user_id', $userId);
+        // })->count();
+
+        return Inertia::render('dashboard', [
+            'total_deliveries' => Inertia::defer(fn() => $totalDeliveries),
+            // 'total_trips' => $totalTrips,
+        ]);
     }
 }
